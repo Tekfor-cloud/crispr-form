fc6e1b12fd945b9a6c8fe7dbb0d65f2783ed0312
Damien Couppé <5243814+Kontrolix@users.noreply.github.com>
2021-02-17 12:07:34 +0100

    [DBG] debug instal cripsr-form-test

--- ci/Docker/crispr_form_odoo_ci/container/build.sh
+++ ci/Docker/crispr_form_odoo_ci/container/build.sh
@@ -1,12 +1,5 @@
-apt-get update
 
 pip3 install pytest pytest-odoo
-
-python3 /container/tools/clone_dependencies.py /home/odoo/addons 12.0
-
-apt-get -yq clean && \
-apt-get -yq autoremove && \
-rm -rf /var/lib/apt/lists/* \
 
 cd /home/odoo/addons
 git clone --branch ${CI_COMMIT_REF_NAME} --single-branch https://gitlab-ci-token:${CI_JOB_TOKEN}@git.lease4.net/article714/crispr-form.git
