1746d0a7d7ad1cb5fd5504316f102d5f523e5b7e
Damien Couppé <5243814+Kontrolix@users.noreply.github.com>
2021-03-31 11:49:04 +0200

    [IMP] Improve multi inheritance

--- modules/crispr_form/ir_ui_view.py
+++ modules/crispr_form/ir_ui_view.py
@@ -6,18 +6,6 @@ class IrUiView(models.Model):
 
     _inherit = "ir.ui.view"
 
-    @api.multi
-    def read_combined(self, fields=None):
-        view_data = super(IrUiView, self).read_combined(fields)
-
-        if view_data.get("type") == "form":
-            arch = self.apply_view_form_arch_insert(
-                etree.fromstring(view_data["arch"])
-            )
-            view_data["arch"] = etree.tostring(arch, encoding="unicode")
-
-        return view_data
-
     def apply_view_form_arch_insert(self, arch):
 
         for insert_arch_tag in arch.xpath("//form-arch-insert"):
@@ -29,3 +17,10 @@ class IrUiView(models.Model):
             parent_tag.replace(insert_arch_tag, view_arch_tree)
 
         return arch
+
+    @api.model
+    def apply_view_inheritance(self, source, source_id, model, root_id=None):
+        source = self.apply_view_form_arch_insert(source)
+        return super(IrUiView, self).apply_view_inheritance(
+            source, source_id, model, root_id
+        )
