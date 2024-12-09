4e18708f5b7b8b9a9003fb8e72a9d63e8de8ad3c
Simon COLLIN <simon.collin@tekfor.cloud>
2024-12-09 15:40:01 +0100

    not using depecrated

--- modules/crispr_form/ir_ui_view.py
+++ modules/crispr_form/ir_ui_view.py
@@ -8,7 +8,7 @@ class IrUiView(models.Model):
     def apply_view_form_arch_insert(self, arch):
         for insert_arch_tag in arch.xpath("//form-arch-insert"):
             view = self.env.ref(insert_arch_tag.get("view_ref"))
-            view_arch = view.read_combined(["arch", "type"])["arch"]
+            view_arch = view.get_combined_arch()
             view_arch_tree = etree.fromstring(view_arch)
 
             parent_tag = insert_arch_tag.getparent()
